ff0b2d3e028b79c1f4b3990eb0415868d249adfc
Tom Grant <thomas.grant.mail@gmail.com>
2022-07-31 01:34:32 +0100

    (*≧∀≦*) MORE Colorfulness, LESS repetition, BIG refactor!!!
    Window open/ close events are now recorded, this allows the next theme
    to be dynamically selected based on the themes of the currently open
    windows. At the moment, the selection mechanism chooses the least
    recently shown, under-represented theme.

--- background.js
+++ background.js
@@ -1,58 +1,50 @@
-const THEMES = [
-    {
-        colors: {
-            frame: '#ec5f67',
-            tab_background_text: '#111',
-        }
-    },
-    {
-        colors: {
-            frame: '#f99157',
-            tab_background_text: '#111',
-        }
-    },
-    {
-        colors: {
-            frame: '#fac863',
-            tab_background_text: '#111',
-        }
-    },
-    {
-        colors: {
-            frame: '#99c794',
-            tab_background_text: '#111',
-        }
-    },
-    {
-        colors: {
-            frame: '#5fb3b3',
-            tab_background_text: '#111',
-        }
-    },
+class BasicColorTheme {
+    constructor(frame, tab_background_text = '#111') {
+        this.frame = frame;
+        this.tab_background_text = tab_background_text;
+        this.usage = 0;
+        this.lastUsed = Math.random();
+    }
 
-    {
-        colors: {
-            frame: '#6699cc',
-            tab_background_text: '#111',
-        }
-    },
-    {
-        colors: {
-            frame: '#c594c5',
-            tab_background_text: '#111',
-        }
-    },
+    get browserThemeObject() {
+        return {
+            colors: {
+                frame: this.frame,
+                tab_background_text: this.tab_background_text,
+            }
+        };
+    }
+}
+
+let themeOfWindowID = new Map();
+const ALL_THEMES = [
+    new BasicColorTheme('#ec5f67'),
+    new BasicColorTheme('#f99157'),
+    new BasicColorTheme('#fac863'),
+    new BasicColorTheme('#99c794'),
+    new BasicColorTheme('#5fb3b3'),
+    new BasicColorTheme('#6699cc'),
+    new BasicColorTheme('#c594c5'),
 ];
 
-let lastThemeIndex = Math.floor(Math.random() * THEMES.length);
-
 function getNextTheme() {
-    lastThemeIndex = (lastThemeIndex + 1) % THEMES.length;
-    return THEMES[lastThemeIndex];
+    const sortedThemes = [...ALL_THEMES];
+    sortedThemes.sort((a, b) => {
+        if (a.usage == b.usage) {
+            return a.lastUsed > b.lastUsed;
+        }
+        return a.usage > b.usage;
+    });
+    return sortedThemes[0];
 }
 
 function applyThemeToWindow(window) {
-    browser.theme.update(window.id, getNextTheme());
+    const newTheme = getNextTheme();
+    browser.theme.update(window.id, newTheme.browserThemeObject);
+
+    newTheme.usage += 1;
+    newTheme.lastUsed = Date.now();
+    themeOfWindowID.set(window.id, newTheme);
 }
 
 async function applyThemeToAllWindows() {
@@ -61,6 +53,13 @@ async function applyThemeToAllWindows() {
     }
 }
 
+function freeThemeOfDestroyedWindow(window_id) {
+    const theme = themeOfWindowID.get(window_id);
+    theme.usage -= 1;
+    themeOfWindowID.delete(window_id);
+}
+
 browser.windows.onCreated.addListener(applyThemeToWindow);
+browser.windows.onRemoved.addListener(freeThemeOfDestroyedWindow);
 browser.runtime.onStartup.addListener(applyThemeToAllWindows);
 browser.runtime.onInstalled.addListener(applyThemeToAllWindows);
